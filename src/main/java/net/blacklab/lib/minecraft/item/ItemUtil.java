de55b0457e6b8cdba148b657cd39c16ee3e33415
arcusmaximus <a@a.com>
2017-01-15 14:05:09 +0100

    Update to MC 1.11

--- src/main/java/net/blacklab/lib/minecraft/item/ItemUtil.java
+++ src/main/java/net/blacklab/lib/minecraft/item/ItemUtil.java
@@ -4,25 +4,19 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.FMLControlledNamespacedRegistry;
+import net.minecraftforge.fml.common.registry.GameData;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ItemUtil {
 	
 	public static Item getItemByStringId(String id){
-		String modid,name = "";
-		if(name.indexOf(":")==-1){
-			modid= "minecraft";
-			name = id;
-		}else{
-			modid= id.split(":")[0];
-			name = id.split(":")[1];
-		}
-		Item item = GameRegistry.findItem(modid, name);
-		return item;
+		return Item.REGISTRY.getObject(new ResourceLocation(id));
 	}
 
 	public static boolean isHelm(ItemStack stack){
-		if(stack!=null){
+		if(!stack.isEmpty()){
 			if(stack.getItem() instanceof ItemArmor){
 				if(((ItemArmor)stack.getItem()).armorType == EntityEquipmentSlot.HEAD){
 					return true;
